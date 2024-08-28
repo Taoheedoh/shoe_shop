91a73b1d83a482442f34a080779c28f90fb7cc77
Taoheedoh <taoheedoh@gmail.com>
2024-08-28 17:18:27 +0100

    Add ColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,22 @@ import 'package:shoe_shop/home_page.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Shopping App',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+        ),
+      ),
+      home: const HomePage(),
     );
   }
 }
